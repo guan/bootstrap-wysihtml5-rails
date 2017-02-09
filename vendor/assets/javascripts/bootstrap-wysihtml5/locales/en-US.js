2feee5f67a13281f1c6b3101d65a951747215fac
guan <kuniyasu.guan@gmail.com>
2017-02-09 16:36:09 +0900

    Update en-US.js

--- vendor/assets/javascripts/bootstrap-wysihtml5/locales/en-US.js
+++ vendor/assets/javascripts/bootstrap-wysihtml5/locales/en-US.js
@@ -1,61 +1,57 @@
 /**
- * English translation for bootstrap-wysihtml5
+ * Japanese translation for bootstrap-wysihtml5
  */
 (function (factory) {
     if (typeof define === 'function' && define.amd) {
         // AMD. Register as an anonymous module.
-        define('bootstrap.wysihtml5.en-US', ['jquery', 'bootstrap.wysihtml5'], factory);
+        define('bootstrap.wysihtml5.ja-JP', ['jquery', 'bootstrap.wysihtml5'], factory);
     } else {
         // Browser globals
         factory(jQuery);
     }
-}(function ($) {
-  $.fn.wysihtml5.locale.en = $.fn.wysihtml5.locale['en-US'] = {
-    font_styles: {
-      normal: 'Normal text',
-      h1: 'Heading 1',
-      h2: 'Heading 2',
-      h3: 'Heading 3',
-      h4: 'Heading 4',
-      h5: 'Heading 5',
-      h6: 'Heading 6'
-    },
-    emphasis: {
-      bold: 'Bold',
-      italic: 'Italic',
-      underline: 'Underline',
-      small: 'Small'
-    },
-    lists: {
-      unordered: 'Unordered list',
-      ordered: 'Ordered list',
-      outdent: 'Outdent',
-      indent: 'Indent'
-    },
-    link: {
-      insert: 'Insert link',
-      cancel: 'Cancel',
-      target: 'Open link in new window'
-    },
-    image: {
-      insert: 'Insert image',
-      cancel: 'Cancel'
-    },
-    html: {
-      edit: 'Edit HTML'
-    },
-    colours: {
-      black: 'Black',
-      silver: 'Silver',
-      gray: 'Grey',
-      maroon: 'Maroon',
-      red: 'Red',
-      purple: 'Purple',
-      green: 'Green',
-      olive: 'Olive',
-      navy: 'Navy',
-      blue: 'Blue',
-      orange: 'Orange'
-    }
-  };
+}(function($){
+    $.fn.wysihtml5.locale["ja-JP"] = {
+        font_styles: {
+            normal: "通常の文字",
+            h1: "見出し1",
+            h2: "見出し2",
+            h3: "見出し3"
+        },
+        emphasis: {
+            bold: "太字",
+            italic: "斜体",
+            underline: "下線"
+        },
+        lists: {
+            unordered: "点字リスト",
+            ordered: "数字リスト",
+            outdent: "左寄せ",
+            indent: "右寄せ"
+        },
+        link: {
+            insert: "リンクの挿入",
+            cancel: "キャンセル"
+        },
+        image: {
+            insert: "画像の挿入",
+            cancel: "キャンセル"
+        },
+        html: {
+            edit: "HTMLを編集"
+        },
+        colours: {
+            black: "黒色",
+            silver: "シルバー",
+            gray: "グレー",
+            maroon: "栗色",
+            red: "赤色",
+            purple: "紫色",
+            green: "緑色",
+            olive: "オリーブ",
+            navy: "ネイビー",
+            blue: "青色",
+            orange: "オレンジ"
+        }
+
+    };
 }));
